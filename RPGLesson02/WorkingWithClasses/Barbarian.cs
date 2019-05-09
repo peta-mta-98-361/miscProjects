7df2cd5d5f3a8ab4ae193b38f693a85e764cbc4a
Michael Chittenden <michaeljchittenden@gmail.com>
2019-05-09 14:11:53 +0100

    SecoindUodate

---
+++ RPGLesson02/WorkingWithClasses/Barbarian.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkingWithClasses
+{
+    class Barbarian : livingThing_Ver5
+    {
+        public override int getArmour()
+        {
+            return 20 - Dexterity / 2;
+        }
+
+        public void renameType(string newType)
+        {
+            type = newType;
+        }
+
+
+    }
+
+}
